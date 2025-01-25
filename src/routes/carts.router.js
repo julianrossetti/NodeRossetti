aa22b61c09ed22aecf0027e03674a5fae1eec6ef
Julian Rossetti <julian.rossetti42@gmail.com>
2025-01-25 16:29:10 -0300

    inicializando proyecto

---
+++ src/routes/carts.router.js
@@ -0,0 +1,18 @@
+import {Router} from 'express';
+import productManager from '../fileManager/productManager.js';
+import cartManager from '../fileManager/cartManager.js';
+
+const router = Router();
+
+/** Método POST: PARA CREAR UN RECURSO
+ * Crear un carrito - El carrito tiene un array de products
+ */
+
+/** Método GET: PARA OBTENER UN RECURSO
+ * Retornar todos los productos de un carrito
+ */
+
+/** Método POST: PARA AGREGAR UN PRODUCTO AL CARRITO */
+
+
+export default router;
